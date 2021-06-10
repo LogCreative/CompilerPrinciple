5f9d389a72d637c1434e782e2f46d8ef3ce20ca3
LogCreative <longlaoda123456@qq.com>
2021-06-10 23:52:01 +0800

    &str store

--- OPG/opg/src/main.rs
+++ OPG/opg/src/main.rs
@@ -1,12 +1,53 @@
 use std::env;
 use std::fs;
 
+struct Production {
+    left: String,
+    right: Vec<String>
+}
+
+struct Opg {
+    contents: String,
+    productions: Vec<Production>
+}
+
+impl Opg {
+    pub fn new(_contents: String) -> Opg {
+        let mut prod:Vec<Production> = Vec::new();
+        for line in _contents.lines(){
+            let ps:Vec<_> = line.split("->").collect();
+            let vs:Vec<_> = ps[1].split_whitespace().collect();
+            prod.push(Production {
+                left: ps[0].trim().to_string(),
+                right: vs.iter().map(|s| s.clone().to_string()).collect()
+            });
+        }
+        Opg{
+            contents: _contents,
+            productions: prod
+        }
+    }
+
+    pub fn generate(&self) -> String {
+        let prod = &self.productions;
+        for prod in prod{
+            println!("{}->{:?}",prod.left,prod.right);
+        }
+        let table = self.contents.clone();
+        table
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     // File for input
     let filename = &args[1];
-    // contents of the file
-    let contents = fs::read_to_string(filename).expect("No such file."); 
-
-    println!("Text in file:\n{}",contents);
+    // Contents of the file
+    let _contents = fs::read_to_string(filename).expect("No such file."); 
+    // Define the struct
+    let opg = Opg::new(_contents);
+    // Get the table
+    let table = opg.generate();
+    // Output the table
+    println!("{}", table);
 }
