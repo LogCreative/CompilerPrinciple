f5b02ecc8484152b9c28e1e5039b52eb011b0878
LogCreative <longlaoda123456@qq.com>
2021-06-11 17:21:29 +0800

    more definition on functions

--- opg/src/main.rs
+++ opg/src/main.rs
@@ -3,6 +3,12 @@ use std::collections::HashMap;
 use std::env;
 use std::fs;
 
+enum RELATION {
+    EQUAL,
+    LESS,
+    GREATER
+}
+
 struct Production {
     left: String,
     right: Vec<String>,
@@ -12,8 +18,31 @@ struct Production {
 /// Generate FIRSTVT set for
 /// every non-terminals.
 ///
-fn gen_firstvt(productions: &Vec<Production>, nt: &HashSet<String>) -> HashMap<String, HashSet<String>>{
+fn gen_firstvt(productions: &Vec<Production>, nts: &HashSet<String>) -> HashMap<String, HashSet<String>>{
     let mut firstvtmap: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut firstvtcon: HashMap<String, Vec<String>> = HashMap::new();
+
+    // Find mono terminal and
+    // record the containing part
+    for p in productions {
+        if !nts.contains(p.right.first().unwrap()) {
+            // Case 1: U => Ty
+            let vts = firstvtmap.entry(p.left.to_string()).or_insert(HashSet::new());
+            vts.insert(p.right.first().unwrap().to_string());
+        } else {
+            // Case 2: U => U_1Ty
+            
+        }
+    }
+
+    // Eliminate loop on 
+    // the containing recursive tree
+    // DFS, the element on the loop
+    // shares the same set.
+
+    // Compose all the sets
+    // by recursing.
+
     firstvtmap
 }
 
@@ -21,7 +50,7 @@ fn gen_firstvt(productions: &Vec<Production>, nt: &HashSet<String>) -> HashMap<S
 /// Generate LASTVT set for
 /// every non-terminals.
 ///
-fn gen_lastvt(productions: &Vec<Production>, nt: &HashSet<String>) -> HashMap<String, HashSet<String>>{
+fn gen_lastvt(productions: &Vec<Production>, nts: &HashSet<String>) -> HashMap<String, HashSet<String>>{
     let mut lastvtmap: HashMap<String, HashSet<String>> = HashMap::new();
     lastvtmap
 }
@@ -63,16 +92,46 @@ fn get_non_terminals(productions: &Vec<Production>) -> HashSet<String> {
 }
 
 ///
+/// Find the equal operators
+///
+fn find_eq(table: &mut HashMap<(String,String),RELATION>, productions: &Vec<Production>) {
+
+}
+
+///
+/// Find the less relations
+///
+fn find_less(table: &mut HashMap<(String,String),RELATION>, productions: &Vec<Production>, firstvt: &HashMap<String,HashSet<String>>) {
+
+}
+
+///
+/// Find the greater relations
+///
+fn find_greater(table: &mut HashMap<(String,String),RELATION>, productions: &Vec<Production>, lastvt: &HashMap<String,HashSet<String>>) {
+
+}
+
+
+///
 /// Generate Operator Precedence Table
 /// for context-free grammar contents.
 ///
 fn opg_generate(contents: &String) -> String {
     let productions: Vec<Production> = gen_productions(&contents);
-    let nt = get_non_terminals(&productions);
-    let firstvt = gen_firstvt(&productions, &nt);
-    let lastvt = gen_lastvt(&productions, &nt);
+    let nts = get_non_terminals(&productions);
+    let firstvt = gen_firstvt(&productions, &nts);
+    let lastvt = gen_lastvt(&productions, &nts);
+    let mut table: HashMap<(String,String),RELATION> = HashMap::new();
 
-    for v in nt{
+    // if there is conflict on operator precedence,
+    // then the grammar is ambiguous.
+    // TODO: panic error
+    find_eq(&mut table, &productions);
+    find_less(&mut table, &productions, &firstvt);
+    find_greater(&mut table, &productions, &lastvt);
+
+    for v in nts{
         println!("{:?}",v);
     }
     contents.clone()
