850e0a7afbc6a52806759d282a63907ffaab661c
LogCreative <longlaoda123456@qq.com>
2021-06-28 17:05:59 +0800

    HW commit

---
+++ filter.ps1
@@ -0,0 +1,18 @@
+Get-ChildItem . '*.tex' -Force -Recurse | ForEach-Object -Process {
+    $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
+
+    (Get-Content $file)|
+        ForEach-Object { $_ -replace '李子龙','Log Creative'} |  # Change Name
+        ForEach-Object { $_ -replace '518070910095',''} | # Change Number
+        Out-File $file -Force
+}
+
+Get-ChildItem .\ -recurse HW*.tex | ForEach-Object -Parallel {
+    $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
+    Set-Location $_.DirectoryName
+    latexmk -pdf $file -interaction=nonstopmode -shell-escape
+}
+
+Get-ChildItem .\ -recurse *_518070910095* | ForEach-Object {
+    Rename-Item $_.FullName$_.FullName.Replace("\_李子龙\_518070910095","")
+}
