414993290eb1e616d794514a47f5e37990db647d
LogCreative <longlaoda123456@qq.com>
2021-06-28 17:10:53 +0800

    script update

--- filter.ps1
+++ filter.ps1
@@ -2,8 +2,8 @@ Get-ChildItem . '*.tex' -Force -Recurse | ForEach-Object -Process {
     $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
 
     (Get-Content $file)|
-        ForEach-Object { $_ -replace '李子龙','Log Creative'} |  # Change Name
-        ForEach-Object { $_ -replace '518070910095',''} | # Change Number
+        ForEach-Object { $_ -replace '***','Log Creative'} |  # Change Name
+        ForEach-Object { $_ -replace '*********',''} | # Change Number
         Out-File $file -Force
 }
 
@@ -13,6 +13,6 @@ Get-ChildItem .\ -recurse HW*.tex | ForEach-Object -Parallel {
     latexmk -pdf $file -interaction=nonstopmode -shell-escape
 }
 
-Get-ChildItem .\ -recurse *_518070910095* | ForEach-Object {
-    Rename-Item $_.FullName$_.FullName.Replace("\_李子龙\_518070910095","")
+Get-ChildItem .\ -recurse *_********* | ForEach-Object {
+    Rename-Item $_.FullName$_.FullName.Replace("*************","")
 }
