381f44e08754cfaaaf08683144bd420f0fcd8174
LogCreative <longlaoda123456@qq.com>
2021-06-29 00:27:50 +0800

    update index

--- genindex.ps1
+++ genindex.ps1
@@ -1,13 +1,13 @@
-# run this to make the index of the doc.
+# run this to make the index.html of the doc.
+# may be broken if your system is confused with certain characters.
 
 Copy-Item -Path "./opg/index.html" -Destination "." -Force
 
 (Get-Content "index.html") |
-    ForEach-Object {$_ -replace "\.\./",""} |
-    ForEach-Object {$_ -replace "all.html","opg/html"} |
-    ForEach-Object {$_ -replace "fn.","opg/fn."} |
-    ForEach-Object {$_ -replace "sidebar-","opg/sidebar-"} |
-    ForEach-Object {$_ -replace "struct.","opg/struct."} |
+    ForEach-Object {$_ -replace "\.\.\/",""} |
+    ForEach-Object {$_ -replace "all\.html","opg/all.html"} |
+    ForEach-Object {$_ -replace "fn\.","opg/fn."} |
+    ForEach-Object {$_ -replace "struct\.","opg/struct."} |
     ForEach-Object {$_ -replace "dfs/","opg/dfs/"} |
     ForEach-Object {$_ -replace "table/","opg/table/"} |
     Out-File "index.html" -Force
